ccc5fc516520eda4adcabfafbd234d03cc4a5356
Andrey <frost86@inbox.ru>
2015-02-02 21:58:49 +0300

    Canary build
    UI, external EXIF-parsing. GPS data transfering. Image title and
    description transfer

---
+++ GeoTagCopier/GeoTagCopier CSharp/DescriptionAndGeoTagCopier.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows.Documents;
+using ExifLibrary;
+using XperiCode.JpegMetadata;
+
+namespace GeoTagCopier_CSharp
+{
+    class DescriptionAndGeoTagCopier
+    {
+        private readonly string _sourceFolder;
+        private readonly string _destFolder;
+
+        public DescriptionAndGeoTagCopier(string sourceFolder, string destFolder)
+        {
+            _sourceFolder = sourceFolder;
+            _destFolder = destFolder;
+        }
+
+
+        public void Run(object sender, DoWorkEventArgs doWorkEventArgs)
+        {
+            var errorFiles = new List<string>();
+            var sourceFiles = GetJpegFiles(_sourceFolder);
+            var destFiles = GetJpegFiles(_destFolder);
+
+            var worker = (sender as BackgroundWorker);
+            worker.ReportProgress(0);
+            foreach (var sourceFile in sourceFiles)
+            {
+                try
+                {
+                    var sourceFileName = new FileInfo(sourceFile).Name;
+                    if (!destFiles.Exists(f => new FileInfo(f).Name.Equals(sourceFileName))) 
+                    {
+                        errorFiles.Add(sourceFileName);
+                        continue;
+                    }
+
+                    var destFile = Path.Combine(_destFolder, sourceFileName);
+                    CopyGeoTags(sourceFile, destFile);
+                    ChangeDescription(sourceFile, destFile);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    errorFiles.Add(new FileInfo(sourceFile).Name);
+                }
+                worker.ReportProgress(100 / sourceFiles.Count);
+            }
+
+            doWorkEventArgs.Result = Tuple.Create(sourceFiles, errorFiles);
+        }
+
+        List<string> GetJpegFiles(string folder)
+        {
+            var files = Directory.GetFiles(folder);
+            return files.Where(IsJpeg).ToList();
+        }
+
+        private static bool IsJpeg(string fileName)
+        {
+            var extension = new FileInfo(fileName).Extension.ToLower();
+            return extension == ".jpg" || extension == ".jpeg";
+        }
+
+
+        static void ChangeDescription(string sourceFile, string destFile)
+        {
+            var sourceMetadata = new JpegMetadataAdapter(sourceFile).Metadata;
+            var destAdapter = new JpegMetadataAdapter(destFile);
+            var destMetadata = destAdapter.Metadata;
+            destMetadata.Subject = sourceMetadata.Subject;
+            destMetadata.Title = sourceMetadata.Title;
+            destMetadata.Comments = sourceMetadata.Comments;
+            destMetadata.Keywords = sourceMetadata.Keywords;
+            destMetadata.Rating = sourceMetadata.Rating;
+
+            destAdapter.Save();
+        }
+
+        static void CopyGeoTags(string fileName, string destName)
+        {
+            var sourceExifFile = ExifFile.Read(fileName);
+            var destExifFile = ExifFile.Read(destName);
+            for (int shift = 0; shift < 31; shift++)
+            {
+                int t = (int)IFD.GPS + shift;
+                var tag = (ExifTag)t;
+                if (sourceExifFile.Properties.ContainsKey(tag))
+                {
+                    var gpsValue = sourceExifFile.Properties[tag];
+                    destExifFile.Properties[tag] = gpsValue;
+                }
+            }
+            var tmpName = destName + "_tmp";
+            destExifFile.Save(tmpName, false);
+            File.Delete(destName);
+            File.Move(tmpName, destName);
+
+
+            //var longitude = exifFile.Properties[ExifTag.GPSLongitude] as GPSLatitudeLongitude;
+            //var latitude = exifFile.Properties[ExifTag.GPSLatitude] as GPSLatitudeLongitude;
+            //var altitude = exifFile.Properties[ExifTag.GPSAltitude] as ExifSRational;
+
+
+            //using (var reader = new ExifReader(fileName))
+            //{
+            //    object longitude, latitude, altitude;
+            //    reader.GetTagValue(ExifTags.GPSLatitude, out latitude);
+            //    reader.GetTagValue(ExifTags.GPSLongitude, out longitude);
+            //    reader.GetTagValue(ExifTags.GPSAltitude, out altitude);
+
+            //    Console.WriteLine("{0}, {1}, {2}", altitude, latitude, longitude);
+            //}
+        }
+    }
+}
---
+++ GeoTagCopier/GeoTagCopier CSharp/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using MessageBox = System.Windows.Forms.MessageBox;
+
+namespace GeoTagCopier_CSharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _sourcePath;
+        private string _destPath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_SourceFolder(object sender, RoutedEventArgs e)
+        {
+            var dlg = new FolderBrowserDialog { SelectedPath = _sourcePath };
+            var res = dlg.ShowDialog();
+            if (res == System.Windows.Forms.DialogResult.OK)
+            {
+                _sourcePath = dlg.SelectedPath;
+                SourcePath.Text = _sourcePath;
+            }
+        }
+
+        private void Button_Click_DestFiles(object sender, RoutedEventArgs e)
+        {
+            var dlg = new FolderBrowserDialog {SelectedPath = _sourcePath};
+            var res = dlg.ShowDialog();
+            if (res == System.Windows.Forms.DialogResult.OK)
+            {
+                _destPath = dlg.SelectedPath;
+                DestPath.Text = _destPath;
+            }
+        }
+
+        private void Button_Click_Convert(object sender, RoutedEventArgs e)
+        {
+            //if (Directory.GetFiles(_destPath).Any())
+            //{
+            //    MessageBox.Show("Select empty folder or delete all files in destination",
+            //        "Destination folder is not empty",
+            //        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            //    return;
+            //}
+
+            Progress.Visibility = Visibility.Visible;
+            Total.Visibility = Errors.Visibility = Visibility.Collapsed;
+
+            var worker = new BackgroundWorker {WorkerReportsProgress = true};
+            var copier = new DescriptionAndGeoTagCopier(_sourcePath, _destPath);
+            worker.DoWork += copier.Run;
+            worker.ProgressChanged += WorkerOnProgressChanged;
+            worker.RunWorkerAsync();
+            worker.RunWorkerCompleted += WorkerOnRunWorkerCompleted;
+        }
+
+        private void WorkerOnRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs runWorkerCompletedEventArgs)
+        {
+            if (runWorkerCompletedEventArgs.Error == null)
+                Progress.Content = string.Format("Done :-)");
+            else
+                Progress.Content = string.Format("Error :-(");
+
+            Total.Visibility = Errors.Visibility = Visibility.Visible;
+            var res = runWorkerCompletedEventArgs.Result as Tuple<List<String>, List<String>>;
+            Total.Content = "Total jpeg files: " + res.Item1.Count;
+            Errors.Content = "Including errors: " + res.Item2.Count;
+        }
+
+        private void WorkerOnProgressChanged(object sender, ProgressChangedEventArgs progressChangedEventArgs)
+        {
+            Progress.Content = string.Format("{0}%", progressChangedEventArgs.ProgressPercentage);
+        }
+    }
+}
